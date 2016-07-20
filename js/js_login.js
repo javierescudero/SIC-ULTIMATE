a1fb4c563fe250d812d88f2485fd3a8849b36ac8
Javier Escudero <j.escudero.g@hotmail.com>
2016-07-20 09:00:40 -0600

    Creacion de archivo index, validacion de login con js y css del login para el posicionamiento del formulario

---
+++ js/js_login.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('#btnAceptar').click(function(){
+		var user = document.getElementById('user').value;
+		var password = document.getElementById('password').value;
+
+		if (user=='' || password=='') {
+			alert('Campos Vacios');
+			window.location='login.html';
+		} else if (user=='admin' && password=='1234') {
+			 window.location='index.html';
+		} else {
+			alert('Usuario o Password Incorrectos');
+			window.location='login.html';
+		}
+	});
+});
