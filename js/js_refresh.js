f98d76cb5ef352bc618a4dfe58acb1516a911ff8
Javier Escudero <j.escudero.g@hotmail.com>
2016-08-03 12:12:33 -0600

    Se quito el intervalo de tiempo de js_refresh.js y los href de los links del menu

--- js/js_refresh.js
+++ js/js_refresh.js
@@ -1,56 +1,45 @@
 $(document).ready(function() {
 	$('#fam_mod').click(function(){
 		location.href = "fam&mod.html";
-		setInterval(100);
 	});
 
 	$('#operaciones').click(function(){
 		location.href = "operaciones.html";
-		setInterval(100);
 	});
 
 	$('#componentes').click(function(){
 		location.href = "componentes.html";
-		setInterval(100);
 	});
 
 	$('#codigos_de_falla').click(function(){
 		location.href = "codigos_de_falla.html";
-		setInterval(100);
 	});
 
 	$('#registros').click(function(){
 		location.href = "registros.html";
-		setInterval(100);
 	});
 
 	$('#desempeno').click(function(){
 		location.href = "desempeno.html";
-		setInterval(100);
 	});
 
 	$('#tendencia').click(function(){
 		location.href = "tendencia.html";
-		setInterval(100);
 	});
 
 	$('#contribuyentes').click(function(){
 		location.href = "contribuyentes.html";
-		setInterval(100);
 	});
 
 	$('#correccion_datos').click(function(){
 		location.href = "correccion_datos.html";
-		setInterval(100);
 	});
 
 	$('#permisos').click(function(){
 		location.href = "permisos.html";
-		setInterval(100);
 	});
 
 	$('#inicio').click(function(){
 		location.href = "index.html";
-		setInterval(100);
 	});
 });
