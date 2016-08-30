130e6177b6c9055042836965ec75977a30676c65
Javier Escudero <j.escudero.g@hotmail.com>
2016-08-30 16:47:53 -0600

    Validando el accceso del usuario y a que area se va a dirigir. Iniciando pruebas con php y BDD

--- js/js_login.js
+++ js/js_login.js
@@ -2,15 +2,23 @@ $(document).ready(function() {
 	$('#btnAceptar').click(function(){
 		var user = document.getElementById('user').value;
 		var password = document.getElementById('password').value;
+		var area = document.getElementById('area').value;
 
-		if (user=='' || password=='') {
+		if (user == '' || password == '') {
 			alert('Campos Vacios');
-			window.location='login.html';
-		} else if (user=='admin' && password=='1234') {
-			 window.location='index.html';
+			window.location = 'login.html';
+		} else if (user == 'admin' && password == '1234') {
+			if (area == 'electronica') {
+				alert('ELECTRONICA');
+			} else if (area == 'electromecanicos') {
+				alert('ELECTROMECANICOS');
+			} else if (area == 'valvulas') {
+				alert('VALVULAS');
+			};
+			//window.location='index.html';
 		} else {
 			alert('Usuario o Password Incorrectos');
-			window.location='login.html';
-		}
+			window.location = 'login.html';
+		};
 	});
 });
