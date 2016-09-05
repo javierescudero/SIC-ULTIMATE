2589e9c523f53ddee2c1928214ef0d29db0bb8f3
javierescudero <j.escudero.g@hotmail.com>
2016-09-04 19:36:07 -0600

    Conexion establecida, login con validacion, inicio de cambios de archivos html por php

--- js/js_login.js
+++ js/js_login.js
@@ -2,29 +2,12 @@ $(document).ready(function() {
 	$('#btnAceptar').click(function(){
 		var user = document.getElementById('user').value;
 		var password = document.getElementById('password').value;
-		var area = document.getElementById('area').value;
 
 		if (user == '' || password == '') {
 			alert('Campos Vacios');
 			window.location = 'login.html';
 		} else if (user == 'admin' && password == '1234') {
-			$.post('php/login.php', {
-				area: area
-			}, function(data, status) {
-				alert(data);
-				alert(status);
-				if (data == 'electronica' ) {
-					alert('Conexion a electronica');
-				} else if (data == 'electromecanicos' ) {
-					alert('Conexion a electromecanicos');
-				} else if (data == 'valvulas' ) {
-					alert('Conexion a valvulas');
-				}
-				else {
-					alert('PROBLEMAS');
-				}
-				//window.location = 'index.html';
-			});
+			window.location = 'index.html';
 		} else {
 			alert('Usuario o Password Incorrectos');
 			window.location = 'login.html';
---
+++ php/conexion.php
@@ -0,0 +1,6 @@
+<?php
+	define("SERVER", "127.0.0.1");
+	define("USER", "root");
+	define("PASSWORD", "root");
+	define("DB", "electronica");
+?>
