b1c618ffde098758c4d314393a95d1ba1abcb983
Javier Escudero <j.escudero.g@hotmail.com>
2016-09-06 09:51:20 -0600

    Se corrigio direccionamiento al validar el usuario

--- php/valida_user.php
+++ php/valida_user.php
@@ -1,21 +1,30 @@
-<?php
-	$usuario = $_POST['user'];
-	$password = $_POST['password'];
+<html>
+<head>
+	<title>Validando Usuario</title>
+	<script src="js/jquery-1.12.4.min.js"></script>
+</head>
+<body>
+	<?php
+		$usuario = $_POST['user'];
+		$password = $_POST['password'];
 
-	if ($usuario == 'admin' && $password == '1234') {
-		echo "<script type='text/javascript'>";
-		echo 	"alert('Acceso Correcto');";
-		echo 	"window.location.href='php/index.php';";
-		echo "</script>";
-	} else if ($usuario == '' || $password == '') {
-		echo "<script type='text/javascript'>";
-		echo 	"alert('Campos Vacios');";
-		echo 	"window.location.href='vacio.html';";
-		echo "</script>";
-	} else {
-		echo "<script type='text/javascript'>";
-		echo 	"alert('Datos Incorrectos');";
-		echo 	"window.location.href='mal.html';";
-		echo "</script>";
-	}
-?>
+		if ($usuario == 'admin' && $password == '1234') {
+			echo "<script type='text/javascript'>";
+			echo 	"alert('Acceso Correcto');";
+			echo 	"window.location.href='php/index.php';";
+			echo "</script>";
+		}
+		if ($usuario == '' || $password == '') {
+			echo "<script type='text/javascript'>";
+			echo 	"alert('Campos Vacios');";
+			echo 	"window.location.href='index.html';";
+			echo "</script>";
+		} else {
+			echo "<script type='text/javascript'>";
+			echo 	"alert('Datos Incorrectos');";
+			echo 	"window.location.href='index.html';";
+			echo "</script>";
+		}
+	?>
+</body>
+</html>
