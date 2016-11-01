ebf1ec75e97d180d06d9e4de1d79eb57428f8e4c
Javier Escudero <j.escudero.g@hotmail.com>
2016-11-01 09:04:20 -0700

    Interfaz de usuario tipo capturista C terminada

---
+++ modulos/capturistaC/php/menu.php
@@ -0,0 +1,31 @@
+<!-- Menu -->
+<div id="menu" data-role="panel" data-position="left" data-position-fixed="false" data-display="reveal">
+	<ul id="ul_menu" class="nav-search" data-role="listview" data-theme="b" data-divider-theme="a">
+		<li data-icon="false" ><a href="#" id="menu_principal">Menu Principal</a></li>
+		<br>
+	</ul>
+	<div id="divMenuPrincipal">
+		<div id="captura" data-role="collapsible" data-collapsed-icon="edit" data-expanded-icon="carat-u">
+			<h3><center>Captura</center></h3>
+			<a href="fam&mod.php" id="fam_mod"  data-role="button" class="ui-btn ui-icon-bars ui-btn-icon-left" data-transition="slide" data-ajax="false">Familias / Modelos</a>
+			<a href="operaciones.php" id="operaciones" data-role="button" class="ui-btn ui-icon-grid ui-btn-icon-left" data-transition="slide" data-ajax="false">Operaciones</a>
+			<a href="componentes.php" id="componentes" data-role="button" class="ui-btn ui-icon-gear ui-btn-icon-left" data-transition="slide" data-ajax="false">Componentes</a>
+			<a href="codigos_de_falla.php" id="codigos_de_falla" data-role="button" class="ui-btn ui-icon-tag ui-btn-icon-left" data-transition="slide" data-ajax="false">Codigos de Falla</a>
+			<a href="registros.php" id="registros" data-role="button" class="ui-btn ui-icon-bullets ui-btn-icon-left" data-transition="slide" data-ajax="false">Registros</a>
+		</div>
+		
+		<div id="reportes" data-role="collapsible" data-collapsed-icon="bars" data-expanded-icon="carat-u">
+			<h3><center>Reportes</center></h3>
+			<a href="correccion_datos.php" id="correccion_datos" data-role="button" title="Correccion de Datos" class="ui-btn ui-icon-edit ui-btn-icon-left" data-transition="slidedown" data-ajax="false">Correccion de Datos</a>
+		</div><br><hr><br>
+
+		<div id="area" data-role="collapsible" data-collapsed-icon="recycle" data-expanded-icon="carat-u">
+			<h3><center>Seleccionar Area</center></h3>
+			<a href="electronica.php" data-role="button" id="electronica" class="ui-btn ui-icon-gear ui-btn-icon-left" data-ajax="false">Electronica</a>
+			<a href="electromecanicos.php" data-role="button" id="electromecanicos" class="ui-btn ui-icon-gear ui-btn-icon-left" data-ajax="false">Electromecanicos</a>
+			<a href="valvulas.php" data-role="button" id="valvulas" class="ui-btn ui-icon-gear ui-btn-icon-left" data-ajax="false">Valvulas</a>
+		</div><br><hr><br>
+		<!--<a href="index.php" data-role="button" id="inicio" class="ui-btn ui-icon-home ui-btn-icon-left" data-ajax="false">Inicio</a>-->
+		<a href="../../../index.php" id="" class="ui-btn ui-icon-power ui-btn-icon-left" data-ajax="false">Salir</a>
+	</div>
+</div>
