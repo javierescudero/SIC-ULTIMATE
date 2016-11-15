2de15fa305ef11abae4a6f0cb06d7ee57ae02b82
Javier Escudero <j.escudero.g@hotmail.com>
2016-11-15 13:41:45 -0700

    Include librerias con links

--- modulos/capturistaA/index.php
+++ modulos/capturistaA/index.php
@@ -12,16 +12,7 @@
 	
 	<title>SIC Ultimate Admin vw1.0</title>
 	
-	<script src="../../js/jquery-1.12.4.min.js"></script>
-	<script src="../../js/jquery.mobile-1.4.5.js"></script>
-	<script src="../../js/highcharts/highcharts.js"></script>
-	<script src="../../js/highcharts/highcharts-3d.js"></script>
-	<script src="../../js/highcharts/dark-unica.js"></script>
-	<script src="../../js/highcharts/exporting.js"></script>
-	<script src="../../js/highcharts/js_graficsAdmin.js"></script>
-	
-	<link rel="stylesheet" href="../../css/jquery.mobile-1.4.5.css">
-	<link rel="stylesheet" href="../../css/css_style.css">
+	<?php include("../../php/librerias.php"); ?>
 </head>
 <style type="text/css">
 	@media screen and (min-width: 480px) {
---
+++ php/librerias.php
@@ -0,0 +1,10 @@
+<script src="../../js/jquery-1.12.4.min.js"></script>
+<script src="../../js/jquery.mobile-1.4.5.js"></script>
+<script src="../../js/highcharts/highcharts.js"></script>
+<script src="../../js/highcharts/highcharts-3d.js"></script>
+<script src="../../js/highcharts/dark-unica.js"></script>
+<script src="../../js/highcharts/exporting.js"></script>
+<script src="../../js/highcharts/js_graficsAdmin.js"></script>
+
+<link rel="stylesheet" href="../../css/jquery.mobile-1.4.5.css">
+<link rel="stylesheet" href="../../css/css_style.css">
